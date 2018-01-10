dc6862b8461359cf7ddba6d6df7532e2d3179a8c
Ethan Brooks <ethanabrooks@gmail.com>
2018-01-10 17:02:17 -0500

    got tensorboard working

--- baselines/ddpg/main.py
+++ baselines/ddpg/main.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
     if MPI.COMM_WORLD.Get_rank() == 0:
         logger.configure()
     try:
-        logger.configure(dir=args.tb_dir, format_strs=['stdout', 'tensorflow'])
+        logger.configure(dir=args['tb_dir'], format_strs=['stdout', 'tensorboard'])
     except AttributeError:
         pass
     # Run actual script.
