aeb0f3463ec4eef74888459b27d163f14a34a406
Ethan Brooks <ethanabrooks@gmail.com>
2018-04-26 17:14:06 -0400

    added random starts with fingers around block

--- environment/pick_and_place.py
+++ environment/pick_and_place.py
@@ -5,7 +5,7 @@ import numpy as np
 from gym import spaces
 from mujoco import ObjType
 
-from environment.base import BaseEnv, at_goal, print1
+from environment.base import BaseEnv, at_goal, print1, distance_between
 from environment.mujoco import MujocoEnv
 
 
@@ -37,6 +37,8 @@ class PickAndPlaceEnv(MujocoEnv):
             steps_per_action=10,
             image_dimensions=None)
 
+        self.initial_qpos = np.copy(self.init_qpos)
+        self._initial_block_pos = np.copy(self._block_pos())
         left_finger_name = 'hand_l_distal_link'
         self._finger_names = [left_finger_name,
                               left_finger_name.replace('_l_', '_r_')]
@@ -46,7 +48,7 @@ class PickAndPlaceEnv(MujocoEnv):
         self.observation_space = spaces.Box(-np.inf, np.inf, shape=(obs_size,), dtype=np.float32)
         self.action_space = spaces.Box(-1, 1, shape=(self.sim.nu - 1,), dtype=np.float32)
         self._table_height = self.sim.get_body_xpos('pan')[2]
-        self._rotation_actuators = ["arm_flex_motor"] # , "wrist_roll_motor"]
+        self._rotation_actuators = ["arm_flex_motor"]  # , "wrist_roll_motor"]
 
         # self._n_block_orientations = n_orientations = 8
         # self._block_orientations = np.random.uniform(0, 2 * np.pi,
@@ -56,13 +58,22 @@ class PickAndPlaceEnv(MujocoEnv):
         # self._current_orienation = None
 
     def reset_qpos(self):
-        # 7.450e-05 - 3.027e-03
-        # 4.385e-01
-        # 1.000e+00
-        # 3.693e-17
-        # 7.590e-19 - 6.184e-04 - 1.101e+00 - 9.759e-07
-        # 3.573e-01
-        # 3.574e-01
+        if np.random.uniform(0, 1) < .5:
+            self.init_qpos = np.array([
+                7.450e-05,
+                - 3.027e-03,
+                4.385e-01,
+                1.000e+00,
+                0,
+                0,
+                - 6.184e-04,
+                - 1.101e+00,
+                0,
+                3.573e-01,
+                3.574e-01,
+            ])
+        else:
+            self.init_qpos = self.initial_qpos
 
         # block_joint = self.sim.jnt_qposadr('block1joint')
 
